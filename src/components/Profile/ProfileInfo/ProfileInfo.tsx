dec1c4e1ffe75ac8262f779dc98aee9f161317f7
Artem <rozen19942810@gmail.com>
2020-11-28 00:51:34 +0200

    refactor

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,27 +1,11 @@
-import React, {ChangeEvent, useState} from "react";
+import React from "react";
 import s from "../ProfileInfo/ProfileInfo.module.scss"
-import {updStatus, ProfileRootType} from "../../../store/ProfileReducer";
+import {ProfileRootType} from "../../../store/ProfileReducer";
 import userIMG from "../../../assets/images/anonymous.svg"
-import SNInput from "../../../common/common_component/input/SNInput";
-import {useDispatch} from "react-redux";
+import ProfileStatus from "./ProfileStatus/ProfileStatus";
 
 
 const ProfileInfo = React.memo((props: ProfileRootType) => {
-
-    const [editMode, setEditMode] = useState(false)
-    const [value, setValue] = useState<string>(props.status ? props.status : '')
-    const dispatch = useDispatch()
-
-    const changeEditMode = () => {
-        setEditMode(true)
-    }
-    const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.currentTarget.value)
-    }
-    const saveStatus = () => {
-        dispatch(updStatus(value))
-        setEditMode(false)
-    }
 
     return (
         <div className={s.profileInfoBlock}>
@@ -33,13 +17,7 @@ const ProfileInfo = React.memo((props: ProfileRootType) => {
                     <span>{props.fullName}</span>
                 </div>
                 <div className={s.status}>
-                    {editMode
-                        ? <div>
-                            <SNInput value={value} onChange={onInputChange} autoFocus={true}/>
-                            <button onClick={saveStatus}>save</button>
-                        </div>
-                        : <span style={{cursor: "pointer"}} onDoubleClick={changeEditMode}>{props.status || '...'}</span>
-                    }
+                    <ProfileStatus status={props.status}/>
                 </div>
             </div>
         </div>
--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
+import {useDispatch} from "react-redux";
+import {updStatus} from "../../../../store/ProfileReducer";
+import SNInput from "../../../../common/common_component/input/SNInput";
 
 
-const ProfileStatus = () => {
+const ProfileStatus = ({status}: { status: string | null }) => {
+
+    const [editMode, setEditMode] = useState(false)
+    const [value, setValue] = useState<string>(status ? status : '')
+    const dispatch = useDispatch()
+
+    const changeEditMode = () => {
+        setEditMode(true)
+    }
+    const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
+    const saveStatus = () => {
+        dispatch(updStatus(value))
+        setEditMode(false)
+    }
+
 
     return (
         <div>
-
+            {editMode
+                ? <div>
+                    <SNInput value={value} onChange={onInputChange} autoFocus={true}/>
+                    <button onClick={saveStatus}>save</button>
+                </div>
+                : <span style={{cursor: "pointer"}} onDoubleClick={changeEditMode}
+                        title={'Double click to edit'}>{status || '...'}</span>
+            }
         </div>
     )
 }
