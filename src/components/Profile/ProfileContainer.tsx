b335ebfb7c67cb38e5f07ac12071163c16ba1826
Artem <rozen19942810@gmail.com>
2020-11-29 23:49:33 +0200

    upd

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -12,25 +12,26 @@ import MiniPreloader from "../../common/common_component/Preloader/MiniPreloader
 const ProfileContainer = React.memo(() => {
 
     const {userID} = useParams<{ userID: string }>()
-
     const dispatch = useDispatch()
     const profile = useSelector<StateType, ProfileRootType>(state => state.profile)
     const authUser = useSelector<StateType, number | null>(state => state.auth.data.id)
     const isAuth = useSelector<StateType, boolean>(state => state.auth.isAuth)
+
     const ID = userID ? +userID : authUser
 
     useEffect(() => {
         ID && dispatch(getProfile(ID))
     }, [ID, dispatch])
 
-    if (!isAuth && !ID) return <Redirect to={'/login'} />
+    // const history = useHistory()
+    // if (!ID) history.push('/login')
+    if (!isAuth && !ID) return <Redirect to={'/login'}/>
     return (
         <div className={s.profileBlock}>
             {profile.profileFetching
                 ? <MiniPreloader/>
                 : <><ProfileInfo {...profile}/>
-                    {authUser === profile.userId && <MyPosts/>}
-                </>
+                    {authUser === profile.userId && <MyPosts/>}</>
             }
         </div>
     )
